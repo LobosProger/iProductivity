cdbcc746edba045c22ec3b7528591c3080ad8e02
Yaroslav Knyazev <78168123+LobosProger@users.noreply.github.com>
2025-05-31 23:25:49 +0500

    upd code

---
+++ Assets/Scripts/Timer logic/TimerCircularSlider.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
+
+public class TimerCircularSlider : MonoBehaviour
+{
+	[SerializeField] private int minutesSteps = 20;
+	[SerializeField] private TMP_Text sliderValueInMinutes;
+	[SerializeField] private Image fillAmountLineOfSlider;
+    [SerializeField] private UI_Knob _radialSlider;
+
+    public static event Action<int> onSetTimerOnMinutes; 
+	
+    private int _currentMinutesSet = 0;
+
+	private void Start()
+	{
+		_radialSlider.OnValueChanged.AddListener(OnSlidedHandlerOnSlider);
+
+		ProcessSliderValuesAndShowToView();
+		onSetTimerOnMinutes?.Invoke(_currentMinutesSet);
+	}
+
+	private void ProcessSliderValuesAndShowToView()
+	{
+		var knobValue = _radialSlider.KnobValue;
+		fillAmountLineOfSlider.fillAmount = knobValue;
+		
+		_currentMinutesSet = (int)(Mathf.Floor(knobValue * _radialSlider.SnapStepsPerLoop) * minutesSteps);
+		sliderValueInMinutes.text = _currentMinutesSet.ToString();
+	}
+
+	private void OnSlidedHandlerOnSlider(float value)
+	{
+		ProcessSliderValuesAndShowToView();
+		onSetTimerOnMinutes?.Invoke(_currentMinutesSet);
+	}
+}
